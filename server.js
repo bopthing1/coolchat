476e37580896b5bd9256009d7fd12f453dfc9f7f
bopthing1 <bopthing1@gmail.com>
2023-05-18 15:37:45 -0700

    e

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+
+const { io } = require("socket.io-client");
+
 const cors = require("cors");
 const express = require("express");
 const http = require("http");
@@ -10,6 +13,8 @@ const ALLOWED_SYMBOLS = ["_"];
 
 const app = express();
 const server = http.createServer(app);
+
+const bcrypt = require("bcrypt");
 
 app.use(cors());
 
@@ -39,12 +44,16 @@ async function doDBAction(action, method) {
 	return dataRes;
 }
 
-const io = new Server(server, {
+const serverIo = new Server(server, {
 	cors: {
 		origin: "http://localhost:3000",
 		methods: ["GET", "POST"],
 	},
 });
+
+const dbSocket = io(REPL_LINK);
+
+dbSocket.emit("yessir");
 
 function generateUserId() {
 	return Math.floor(100000000 + Math.random() * 900000000);
@@ -202,50 +211,40 @@ async function updateMyChannels(socket, accountId) {
 	socket.emit("updateMyChannels", myChannels);
 }
 
-io.on("connection", async (socket) => {
+serverIo.on("connection", async (socket) => {
 	console.log("new connection!");
-
 	socket.emit("checkLoginLocalstorage");
-
 	socket.on("isChannelValid", async (channelId) => {
 		const channel = await doDBAction("get/C_" + channelId);
-
 		if (channel) {
 			socket.emit("channelValidSuccess", channel);
 		} else {
 			socket.emit("channelValidFail");
 		}
 	});
-
 	socket.on("signup", async (data) => {
 		const username = data.username;
 		const password = data.password;
-
 		await validateSignup(username, password)
 			.then(async (message) => {
 				socket.emit("credStatus", {
 					status: message,
 					err: false,
 				});
-
 				console.log(message);
-
 				const userData = {
 					username: username,
 					password: password,
 					accountId: generateUserId(),
 					channelsJoined: [],
 				};
-
 				// const get = await doDBAction("list");
 				// console.log(get);
 				const result = await doDBAction(
 					`set/U_${userData.accountId}/${JSON.stringify(userData)}`,
 					"POST"
 				);
-
 				// console.log(result);
-
 				socket.emit("loginSuccess", userData);
 			})
 			.catch(async (why) => {
@@ -256,15 +255,11 @@ io.on("connection", async (socket) => {
 				});
 			});
 	});
-
 	socket.on("login", async (data, auto) => {
 		const username = data.username;
 		const password = data.password;
-
 		const account = await isAccount(username, password);
-
 		// console.log("logging in...");
-
 		if (account) {
 			if (!auto) {
 				socket.emit("credStatus", {
@@ -272,9 +267,7 @@ io.on("connection", async (socket) => {
 					err: false,
 				});
 			}
-
 			// console.log(account);
-
 			socket.emit("loginSuccess", account);
 			await updateMyChannels(socket, account.accountId);
 		} else {
@@ -286,28 +279,22 @@ io.on("connection", async (socket) => {
 			}
 		}
 	});
-
 	socket.on("logout", () => {
 		socket.emit("logoutSuccess");
 	});
-
 	socket.on("newChannel", async (data) => {
 		const title = data.title;
 		const description = data.title;
 		const owner = data.owner;
 		const private = data.private;
-
 		console.log("new channel");
-
 		await validateChannel(title, owner)
 			.then(async (message) => {
 				socket.emit("channelStatus", {
 					status: message,
 					err: false,
 				});
-
 				socket.emit("channelSuccess");
-
 				const channelData = {
 					title: title,
 					description: description,
@@ -318,14 +305,11 @@ io.on("connection", async (socket) => {
 					private: private,
 					channelId: generateUserId(),
 				};
-
 				await doDBAction(
 					`set/C_${channelData.channelId}/${JSON.stringify(channelData)}`,
 					"POST"
 				);
-
 				await addChannelToPersonChannelList(channelData.channelId, owner);
-
 				await updateMyChannels(socket, owner);
 			})
 			.catch(async (err) => {
@@ -336,61 +320,48 @@ io.on("connection", async (socket) => {
 				});
 			});
 	});
-
 	socket.on("hasPermsForChannel", async (accountId, channelId) => {
 		channelId = parseInt(channelId);
 		accountId = parseInt(accountId);
 		let channel = await doDBAction("get/C_" + channelId);
 		channel = JSON.parse(channel);
-
 		if (channel) {
 			const members = channel.members;
 			// console.log(channel);
-
 			let success = false;
-
 			members.forEach((member) => {
 				member = parseInt(member);
 				// console.log(members, typeof member, typeof accountId)
-
 				if (member === accountId) {
 					console.log("yay! member is accountId");
 					socket.emit("hasPermsForChannelSuccess", channel);
 					success = true;
 				}
 			});
-
 			if (!success) {
 				socket.emit("hasPermsForChannelFailed", channel);
 			}
 		}
 	});
-
 	socket.on("getValidAccount", async (username, password) => {
 		const account = await isAccount(username, password);
-
 		if (account) {
 			socket.emit("getValidAccountSuccess", account);
 		} else {
 			socket.emit("getValidAccountFail");
 		}
 	});
-
 	socket.on("message", async (data) => {
 		if (data.content && data.author && data.channelId) {
 			const channel = await doDBAction("get/C_" + data.channelId);
-
 			if (channel) {
 				const messageData = {
 					content: data.content,
 					author: data.author,
 				};
-
 				const messages = channel.messages;
 				messages.push(messageData);
-
 				await doDBAction(`set/C_${data.channelId}/${JSON.stringify(channel)}`);
-
 				socket.emit("updateMessages", {
 					channel: data.channelId,
 					messages: messages,
